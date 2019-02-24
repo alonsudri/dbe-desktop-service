2e65d8087568d6cce5eaa32b996748aedbc8d83e
czalexpic <czalexpic@gmail.com>
2019-02-24 18:46:37 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,140 @@
+#!/usr/bin/env node
+const path = require('path')
+const url = require('url')
+const util = require('util')
+
+const HttpsProxyAgent = require('https-proxy-agent')
+const WebSocket = require('ws')
+const axios = require('axios')
+const _ = require('lodash')
+
+axios.interceptors.request.use(function (config) {
+  if (config.method === 'get') config.url += `?v${_.random(0, 100000)}${(new Date()).getTime()}`
+  console.log(config.url)
+  return config
+}, function (error) {
+  return Promise.reject(error)
+})
+
+const SERVER = `wss://${process.env.SERVER || 'db-engine-service.azurewebsites.net'}:443`
+const CLIENT = process.env.CLIENT || `${Math.random()}${(new Date()).getTime()}`.replace(/\./g, '')
+const PROXY = process.env.PROXY
+const AGENT = (PROXY) ? new HttpsProxyAgent(url.parse(PROXY)) : null
+const updater = require('./updater')
+
+// :TODO MDS = MODULES
+let ws
+let allowReconnect = false
+
+const updateStatus = function (Msg) {
+  if (ws) ws.send(JSON.stringify({ msgType: 'onUpdateStatus', msg: Msg }))
+}
+
+const MDS = {
+  // constants
+  SERVER: SERVER,
+  VERSION: require('./package').version,
+  DIR_HOME: process.env.DIR_HOME,
+  DIR_APP: process.env.DIR_APP,
+
+  path: path,
+  url: url,
+  util: util,
+  fs: require('fs'),
+  fse: require('fs-extra'),
+  https: require('https'),
+  unzipStream: require('unzip-stream'),
+  exec: util.promisify(require('child_process').exec),
+  timeOut: util.promisify(setTimeout),
+  axios: axios,
+  _: _,
+  updateStatus: updateStatus
+}
+
+function ErrorConnected (e) {
+  console.log(e)
+  if (allowReconnect) {
+    allowReconnect = false
+    console.log('websocket lost connect to', SERVER, 'reconnect after 5s')
+    ws = null
+    setTimeout(Connect, 5000)
+  }
+}
+
+function Connect () {
+  allowReconnect = true
+  console.log('websocket start connected to', SERVER)
+  if (AGENT) {
+    ws = new WebSocket(SERVER, { agent: AGENT, rejectUnauthorized: false })
+  } else {
+    ws = new WebSocket(SERVER, { rejectUnauthorized: false })
+  }
+  ws.onopen = function () {
+    console.log('websocket is connected ...')
+    ws.send(JSON.stringify({
+      msgType: 'onOpenConnection',
+      msg: { type: 'client', id: CLIENT, status: 'connected' }
+    }))
+    // TODO: remove prev versions
+    updater.checkRemovePreviousVersion(MDS)
+  }
+  ws.onmessage = function (ev) {
+    let data = JSON.parse(ev.data)
+    if (data.msgType === 'onSendCommand') {
+      if (data.msg.command === 'gitTasks') new gitTasks(data)
+      if (data.msg.command === 'checkUpdate') updater.checkVersion(MDS)
+    }
+  }
+  ws.onclose = ErrorConnected
+  ws.onerror = ErrorConnected
+}
+
+const TASK = function (Task) {
+  return new Promise((resolve, reject) => {
+    axios
+      .get(Task.git_url, { responseType: 'text' })
+      .then(response => {
+        try {
+          let script = response.data
+          let func = new Function('parameters, modules', `return ${script.slice(script.indexOf('new Promise'))}`)
+          func(Task.parameters, MOD)
+            .then(resolve)
+            .catch(reject)
+        } catch (e) {
+          reject('try/catch script, NOTE: the function should always return PROMISE')
+        }
+      })
+      .catch(_ => {
+        reject('error load script')
+      })
+  })
+}
+
+function gitTasks (Msg) {
+  async function asyncTasks (Tasks) {
+    for (let i = 0; i < Tasks.length; i++) {
+      await new TASK(Tasks[i], Msg)
+    }
+    return 'all Done'
+  }
+
+  axios
+  // .get(Msg.msg.url)
+    .get('https://raw.githubusercontent.com/czalexpic/test-tasks/master/tasks.json')
+    .then(response => {
+      asyncTasks(response.data)
+        .then(response => {
+          console.log(response)
+        })
+        .catch(response => {
+          console.log(response)
+        })
+    })
+    .catch(e => {
+      console.log('error load tasks')
+      console.log(e)
+    })
+}
+
+Connect()
+
---
+++ updater.js
@@ -0,0 +1,120 @@
+module.exports.checkVersion = function (MDS) {
+  let check = (MDS) => {
+    MDS.axios
+      .get('https://api.github.com/repos/czalexpic/dbe-client-service/tags')
+      .then(response => {
+        let latest = response.data[0]
+        if (latest) {
+          if (latest.name === MDS.VERSION) {
+            MDS.updateStatus('Update no need')
+          } else {
+            MDS.updateStatus('Start download new version')
+            downloadRelease(latest.name, MDS)
+          }
+        } else {
+          MDS.timeOut(5000).then(_ => {
+            check(MDS)
+          })
+        }
+      })
+  }
+  let downloadRelease = (Version, MDS) => {
+    MDS.https
+      .get(`https://codeload.github.com/czalexpic/dbe-client-service/zip/${Version}`, (response) => {
+        response
+          .on('error', _ => {
+            MDS.updateStatus('Error download new version, try again after 5s')
+            MDS.timeOut(5000).then(_ => {
+              downloadRelease(Version, MDS)
+            })
+          })
+          .pipe(MDS.unzipStream.Extract({ path: MDS.DIR_APP }))
+          .on('finish', _ => {
+            MDS.timeOut(10000).then(_ => {
+              MDS.updateStatus('Update service XML')
+              updateXML(Version, MDS)
+            })
+          })
+      })
+  }
+  let updateXML = (Version, MDS) => {
+    MDS.fse.outputFile(MDS.path.join(MDS.DIR_APP, 'daemon', 'databridgeengineservice.xml'), `
+      <service>
+        <id>databridgeengineservice.exe</id>
+        <name>Data Bridge Engine Service</name>
+        <description>Data Bridge Engine Service</description>
+        <executable>${MDS.path.join(MDS.DIR_APP, `dbe-client-service-${Version}`, 'node.exe')}</executable>
+        <logmode>rotate</logmode>
+        <argument>--harmony</argument>
+        <argument>${MDS.path.join(MDS.DIR_APP, '..', 'resources', 'lib', 'wrapper.js')}</argument>
+        <argument>--file</argument>
+        <argument>${MDS.path.join(MDS.DIR_APP, `dbe-client-service-${Version}`, 'index.js')}</argument>
+          <argument>--log</argument>
+          <argument>Data Bridge Engine Service wrapper</argument>
+          <argument>--grow</argument>
+          <argument>0.25</argument>
+          <argument>--wait</argument>
+          <argument>1</argument>
+          <argument>--maxrestarts</argument>
+          <argument>3</argument>
+          <argument>--abortonerror</argument>
+          <argument>n</argument>
+          <argument>--stopparentfirst</argument>
+          <argument>undefined</argument>
+          <stoptimeout>30sec</stoptimeout>
+          <env name="HOME" value="${MDS.DIR_HOME}"/>
+          <env name="APP" value="${MDS.DIR_APP}"/>
+          <workingdirectory>${MDS.DIR_APP}</workingdirectory>
+        </service>`)
+      .then(_ => {
+        MDS.updateStatus('Restart service')
+        restartService(MDS)
+      })
+      .catch(_ => {
+        MDS.updateStatus('Error download new version, try again after 5s')
+        MDS.timeOut(5000).then(_ => {
+          updateXML(Version, MDS)
+        })
+      })
+  }
+  let restartService = (MDS) => {
+    try {
+      MDS.exec('net stop databridgeengineservice.exe && net start databridgeengineservice.exe')
+        .then(_ => {
+          MDS.updateStatus('Update finish')
+        })
+        .catch(_ => {
+          MDS.updateStatus('Error restart service, try again after 5s')
+          MDS.timeOut(5000).then(_ => {
+            restartService(MDS)
+          })
+        })
+    } catch (e) {
+      MDS.updateStatus('Error restart service, try again after 5s')
+      MDS.timeOut(5000).then(_ => {
+        restartService(MDS)
+      })
+    }
+  }
+  check(MDS)
+}
+module.exports.checkRemovePreviousVersion = function (MDS) {
+  MDS.fse.readJson(MDS.path.join(MDS.DIR_APP, 'config.json'))
+    .then(response => {
+      if (MDS.VERSION !== response.isServiceVersion) {
+        MDS.fse.remove(MDS.path.join(MDS.DIR_APP, `dbe-client-service-${response.isServiceVersion}`))
+          .then(_ => {
+            MDS.fse.writeJson(MDS.path.join(MDS.DIR_APP, 'config.json'), Object.assign(response, { isServiceVersion: MDS.VERSION }))
+              .catch(_ => {
+                MDS.updateStatus('Error update config.json')
+              })
+          })
+          .catch(_ => {
+            MDS.updateStatus('Error remove previous version')
+          })
+      }
+    })
+    .catch(_ => {
+      MDS.updateStatus('Error load config.json')
+    })
+}
