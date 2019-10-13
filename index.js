797684c4751cbb41c14d78e6b8f510a7bf5ba01b
czalexpic <czalexpic@gmail.com>
2019-10-13 04:37:26 +0300

    change

--- index.js
+++ index.js
@@ -22,16 +22,11 @@ module.exports = function () {
   let timeoutConnect;
   let callback;
 
-  const updateStatus = (Msg) => {
-    let msg = {msgType: 'onUpdateStatus', msg: Msg};
-    callback(msg);
-    if (ws) ws.send(JSON.stringify(msg));
-  };
-
   const MDS = {
     // constants
     CLIENT: null,
-    SERVER: 'wss://db-engine-service.azurewebsites.net:443',
+    PROXY: null,
+    SERVER: null,
     VERSION: require('./package').version,
     DIR_HOME: null,
     DIR_APP: null,
@@ -48,7 +43,71 @@ module.exports = function () {
     _: lodash,
     unzipStream: unzipStream,
 
-    updateStatus: updateStatus
+    pathKeyDefault: 'C:\\ProgramData\\Qlik\\Sense\\Repository\\Exported Certificates\\.Local Certificates\\client_key.pem',
+    pathCertDefault: 'C:\\ProgramData\\Qlik\\Sense\\Repository\\Exported Certificates\\.Local Certificates\\client.pem',
+    qlikReq(method = 'GET', endpoint = '/qrs/app') {
+      return new Promise((resolve, reject) => {
+        let key;
+        try {
+          key = fs.readFileSync(MDS.CLIENT.certificateClientKey || MDS.pathKeyDefault);
+        } catch (e) {
+          reject(e.message);
+          return;
+        }
+        let cert;
+        try {
+          cert = modules.fs.readFileSync(MDS.CLIENT.certificateClientCert || MDS.pathCertDefault);
+        } catch (e) {
+          reject(e.message);
+          return;
+        }
+        https.get({
+          rejectUnauthorized: false,
+          hostname: 'localhost',
+          port: 4242,
+          path: endpoint + '?xrfkey=abcdefghijklmnop',
+          method: method,
+          headers: {
+            'X-Qlik-Xrfkey': 'abcdefghijklmnop',
+            'X-Qlik-User': 'UserDirectory=Internal;UserId=sa_repository'
+          },
+          key: key,
+          cert: cert
+        }, function (res) {
+          res.on('data', function (chunk) {
+            resolve(chunk);
+          });
+        }).on('error', function () {
+          reject('Error get app names ' + e.message);
+        })
+      })
+    },
+    writeToLog(ServiceCommandID, Status) {
+      if (ServiceCommandID) {
+        let data = JSON.stringify({
+          serviceCommandID: ServiceCommandID,
+          message: Status,
+          customerLicenseToken: this.CLIENT.customerLicenseToken,
+          engineServiceUserID: this.CLIENT.appUserID,
+          securityToken: this.CLIENT.securityToken,
+        });
+        let req = https.request({
+          hostname: 'api.databridge.ch',
+          path: '/api/EngineServices/WriteToLog',
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': data.length
+          }
+        }, function (res) {
+          res.on('error', _ => {
+            console.log('Error writeToLog')
+          });
+        });
+        req.write(data);
+        req.end();
+      }
+    }
   };
 
   this.init = function (Mds, Callback) {
@@ -57,107 +116,38 @@ module.exports = function () {
     axios.defaults.proxy = (MDS.PROXY.data && MDS.PROXY.data.host && MDS.PROXY.data.port) ? MDS.PROXY.data : false;
     this.connect();
   };
-  this.status = function () {
-    callback({type: 'app', msgType: 'isServiceConnect', msg: isConnected});
-  };
   this.handleErrorConnect = () => {
     callback({type: 'app', msgType: 'isServiceConnect', msg: false});
-    isConnected = false;
     if (allowReconnect) {
+      console.log(`websocket lost connect to ${MDS.SERVER} reconnect after 5s`);
       allowReconnect = false;
       ws = null;
-      updateStatus(`websocket lost connect to ${MDS.SERVER} reconnect after 5s`);
-      callback({msgType: 'disconnect', msg: {}});
       timeoutConnect = setTimeout(this.connect, 5000);
     }
   };
 
-  // TODO: UPDATE
-  this.checkUpdateUser = () => {
-    axios
-      .get(`https://api.databridge.ch/api/Accounts/GetServiceUserByToken/${MDS.CLIENT.appUserID}/${MDS.CLIENT.customerLicenseToken}`)
-      .then(response => {
-        MDS.CLIENT = response.data;
-        updateStatus('user update')
-        callback({type: 'app', msgType: 'updateUser', msg: {data: response.data}});
-        callback({type: 'local', msgType: 'restart'});
-      })
-      .catch(() => {
-        updateStatus('error user update');
-      })
-  };
-  // TODO: GIT TASKS
   this.textTask = (Msg) => {
     return new Promise((resolve, reject) => {
       try {
         let func = new Function('modules', `return ${Msg.msg.jsCode.slice(Msg.msg.jsCode.indexOf('new Promise'))}`);
-        func(MDS).then(resolve).catch(reject)
+        func(MDS)
+          .then(_ => {
+            MDS.writeToLog(Msg.msg.serviceCommandID, 'Done');
+            resolve(`Done: ${Msg.msg.serviceCommandID}`);
+          })
+          .catch(err => {
+            MDS.writeToLog(Msg.msg.serviceCommandID, 'Error');
+            reject(`Error: ${Msg.msg.serviceCommandID}`);
+          })
       } catch (e) {
-        reject('try/catch script, NOTE: the function should always return PROMISE')
+        MDS.writeToLog(Msg.serviceCommandID, 'Error: try/catch script, NOTE: the function should always return PROMISE');
+        reject(err)
       }
     })
   };
-  this.gitTask = (Task) => {
-    return new Promise((resolve, reject) => {
-      console.log('Task');
-      console.log(Task.git_url);
-      let url = Task.git_url;
-      url += `${/\?token/gi.test(url) ? '&' : '?'}v=${(new Date()).getTime()}`;
-      console.log(url);
-      console.log('---');
-      axios
-        .get(url, {responseType: 'text'})
-        .then(response => {
-          try {
-            let script = response.data;
-            let func = new Function('parameters, modules', `return ${script.slice(script.indexOf('new Promise'))}`);
-            func(Task.parameters, MDS)
-              .then(resolve)
-              .catch(reject)
-          } catch (e) {
-            reject('try/catch script, NOTE: the function should always return PROMISE')
-          }
-        })
-        .catch(_ => {
-          reject('error load script')
-        })
-    })
-  };
-  this.gitTasks = (Msg) => {
-    return new Promise((resolve, reject) => {
-      async function asyncTasks(Tasks, self) {
-        for (let i = 0; i < Tasks.length; i++) await self.gitTask(Tasks[i], Msg);
-        return `Task ${Msg.msg.commandName || ''} Done`;
-      }
 
-      console.log('Tasks');
-      console.log(Msg.msg.url);
-      let url = Msg.msg.url;
-      url += `${/\?token/gi.test(url) ? '&' : '?'}v=${(new Date()).getTime()}`;
-      console.log(url);
-      console.log('---');
-      axios
-        .get(url)
-        // .get('https://raw.githubusercontent.com/czalexpic/test-tasks/master/tasks.json')
-        .then(response => {
-          asyncTasks(response.data, this)
-            .then(resolve)
-            .catch(reject)
-        })
-        .catch(() => {
-          reject(`Error load task ${Msg.msg.commandName || ''}`)
-        })
-    })
-  };
-  this.response = (Msg) => {
-    if (Msg.text) {
-      updateStatus(Msg.text);
-    }
-  };
   this.connect = () => {
     allowReconnect = true;
-    isConnected = true;
-    callback({type: 'app', msgType: 'isServiceConnect', msg: true});
     if (MDS.PROXY && MDS.PROXY.host && MDS.PROXY.port) {
       ws = new WebSocket(MDS.SERVER, null, {
         agent: new HttpsProxyAgent(MDS.PROXY),
@@ -167,43 +157,33 @@ module.exports = function () {
       ws = new WebSocket(MDS.SERVER, null, {rejectUnauthorized: false});
     }
     ws.onopen = function () {
-      ws.send(JSON.stringify({
-        msgType: 'onOpenConnection',
-        msg: {type: 'client', id: MDS.CLIENT.appUserID, status: 'connected'}
-      }));
-      callback({msgType: 'connect', msg: {}});
+      ws.send(JSON.stringify({type: 'reg', id: MDS.CLIENT.appUserID}));
+      callback({type: 'app', msgType: 'isServiceConnect', msg: true});
     };
     ws.onmessage = (ev) => {
       let data = JSON.parse(ev.data);
-      if (data.msgType === 'onSendCommand') {
-        if (data.msg.command === 'gitTasks') {
-          updateStatus(`Start run task ${data.msg.commandName || ''}`);
-          this.gitTasks(data).then(updateStatus).catch(updateStatus);
-        }
+      if (data.type === 'command') {
         if (data.msg.command === 'textTask') {
-          updateStatus(`Start run task ${data.msg.commandName || ''}`);
-          this.textTask(data).then(updateStatus).catch(updateStatus);
-        }
-        if (data.msg.command === 'checkUpdate') {
-          // updateStatus('Start service update');
+          this.textTask(data)
+            .then(result => {
+              console.log(result)
+            })
+            .catch(err => {
+              console.log(err, data)
+            })
+        } else if (data.msg.command === 'checkUpdate') {
           callback({type: 'local', msgType: 'serviceDownload'})
-        }
-        if (data.msg.command === 'checkUpdateUser') {
-          updateStatus('Start update user');
-          this.checkUpdateUser();
-        }
-        if (['checkUpdateApp'].includes(data.msg.command)) {
-          callback({type: 'app', msgType: 'checkUpdateApp'});
+        } else if (['checkUpdateUser', 'checkUpdateApp'].includes(data.msg.command)) {
+          callback({type: 'app', msgType: data.msg.command});
         }
       }
-    };
+    }
     ws.onclose = this.handleErrorConnect;
     ws.onerror = this.handleErrorConnect;
   };
 
   this.destroy = () => {
     allowReconnect = false;
-    isConnected = false;
     callback({type: 'app', msgType: 'isServiceConnect', msg: false});
     if (timeoutConnect) {
       clearTimeout(timeoutConnect);
@@ -211,7 +191,6 @@ module.exports = function () {
     }
     if (ws) ws.close();
   };
-  this.updateStatus = updateStatus;
 
   return this;
 };
